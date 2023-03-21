fbd25505ad7874dbdb97ab1cdeeaba4b1aa59605
Abhijeet <aadi18297@gmail.com>
2023-03-21 23:32:13 +0530

    changes

--- app/src/main/java/com/example/wapcosfeedback/fragments/FeedbackListFragment.java
+++ app/src/main/java/com/example/wapcosfeedback/fragments/FeedbackListFragment.java
@@ -48,8 +48,7 @@ public class FeedbackListFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_feedback_list, container, false);
 
